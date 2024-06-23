4cb6ce4dc2bc8eccca993bb8f11dca9ef8d5a97d
Maduri Manikanta <manimaduri2@gmail.com>
2024-06-23 19:02:06 +0530

    moved auth apis to new service

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,29 +1,59 @@
 import express from "express";
-import { UserService } from "../services/UserService";
+import { AuthService } from "../services/AuthService";
 import { errorResponse, successResponse } from "../utils/responseHandler";
+import { UserService } from "../services/UserService";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = express.Router();
+const authService = new AuthService();
 const userService = new UserService();
 
 router.post("/register", async (req, res) => {
   try {
     const user = req.body;
-    const result = await userService.createUser(user);
+    const result = await authService.createUser(user);
     successResponse(res, result, 201);
   } catch (error: any) {
     console.error("Error creating user........:", error.stack);
-    errorResponse(res, error, error?.message ?? "Registration Failed! Please try again.",error?.statusCode || 500);
+    errorResponse(
+      res,
+      error,
+      error?.message ?? "Registration Failed! Please try again.",
+      error?.statusCode || 500
+    );
   }
 });
 
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const result = await userService.authenticateUser(email, password);
+    const result = await authService.authenticateUser(email, password);
     successResponse(res, result, 200);
   } catch (error: any) {
     console.error("Error logging in user........:", error.stack);
-    errorResponse(res, error, error?.message ?? "Login Failed! Please try again.", error?.statusCode || 500);
+    errorResponse(
+      res,
+      error,
+      error?.message ?? "Login Failed! Please try again.",
+      error?.statusCode || 500
+    );
+  }
+});
+
+router.get("/employeesByOrganization/:organizationId",authMiddleware, async (req, res) => {
+  try {
+    const result = await userService.getAllEmployeesByOrganization(
+      req.params.organizationId
+    );
+    successResponse(res, result, 200);
+  } catch (error: any) {
+    console.error("Error fetching employees........:", error.stack);
+    errorResponse(
+      res,
+      error,
+      error?.message ?? "Failed to fetch employees.",
+      error?.statusCode || 500
+    );
   }
 });
 
---
+++ src/services/AuthService.ts
@@ -0,0 +1,171 @@
+import { User } from "../models/User";
+import { UserRepository } from "../repositories/UserRepository";
+import { hashPassword } from "../utils/hashPassword";
+import jwt from "jsonwebtoken";
+import { OrganizationRepository } from "../repositories/OrganizationRepository";
+import { EmployeeRepository } from "../repositories/EmployeeRepository";
+import { plainToClass } from "class-transformer";
+import { CreateOrganizationDTO } from "../models/dto/OrganizationDTO";
+import { EmployeeDTO } from "../models/dto/EmployeeDTO";
+import { validateDTO } from "../utils/validateDTO";
+import { initDB } from "../models";
+import { CreateUserDTO, UserRole } from "../models/dto/UserDTO";
+import { UniqueConstraintError, Transaction } from "sequelize";
+import { HttpError } from "../utils/responseHandler";
+import bcrypt from "bcrypt";
+
+export class AuthService {
+  private userRepository: UserRepository;
+  private organizationRepository: OrganizationRepository;
+  private employeeRepository: EmployeeRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+    this.organizationRepository = new OrganizationRepository();
+    this.employeeRepository = new EmployeeRepository();
+  }
+
+  async createUser(user: Partial<User>) {
+    const sequelize = await initDB();
+    const transaction = await sequelize.transaction();
+    try {
+      const userDTO = await this.prepareUserDTO(user);
+      const createdUser = await this.userRepository.createUser(userDTO, {
+        transaction,
+      });
+      const token = this.generateToken(createdUser, userDTO.role);
+
+      if (user.role === UserRole.ORGANIZATION) {
+        await this.handleOrganizationUser(
+          user,
+          userDTO,
+          createdUser,
+          transaction
+        );
+      } else if (user.role === UserRole.EMPLOYEE) {
+        await this.handleEmployeeUser(user, userDTO, createdUser, transaction);
+      }
+
+      await transaction.commit();
+      return this.formatResponse(createdUser, token);
+    } catch (error) {
+      await transaction.rollback();
+      this.handleError(error);
+    }
+  }
+
+  async authenticateUser(email: string, password: string): Promise<any> {
+    const user = await this.userRepository.findUserByEmail(email);
+    if (!user) {
+      throw new HttpError("User not found", 404);
+    }
+  
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new HttpError("Invalid Credentials", 401);
+    }
+  
+    const token = this.generateToken(user, user.role);
+  
+    return this.formatResponse(user, token);
+  }
+
+  async prepareUserDTO(user: Partial<User>) {
+    const userDTO = plainToClass(CreateUserDTO, user);
+    await validateDTO(userDTO);
+    userDTO.password = await hashPassword(userDTO.password);
+    return userDTO;
+  }
+
+  generateToken(createdUser: User, role: string) {
+    if (!process.env.JWT_SECRET) {
+      throw new HttpError("JWT_SECRET is not defined");
+    }
+    return jwt.sign(
+      { userId: createdUser.id, role: role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRY }
+    );
+  }
+
+  async handleOrganizationUser(
+    user: Partial<User>,
+    userDTO: CreateUserDTO,
+    createdUser: User,
+    transaction: Transaction
+  ) {
+    const organizationDTO = plainToClass(CreateOrganizationDTO, {
+      userId: createdUser.id,
+      ...user,
+    });
+    await validateDTO(organizationDTO);
+    const organizationExists =
+      await this.userRepository.findOrganizationUserByEmailDomain(
+        userDTO.email.split("@")[1]
+      );
+    if (organizationExists) {
+      throw new HttpError("Organization already exists",400);
+    }
+    try {
+      await this.organizationRepository.createOrganization(organizationDTO, {
+        transaction,
+      });
+    } catch (error: any) {
+      console.log("Failed to create organization", error?.message);
+      throw new HttpError(error?.message || "Failed to create organization ", error?.statusCode || 500);
+    }
+  }
+
+  async handleEmployeeUser(
+    user: Partial<User>,
+    userDTO: CreateUserDTO,
+    createdUser: User,
+    transaction: Transaction
+  ) {
+    const emailDomain = userDTO.email.split("@")[1];
+    const organizationUser =
+      await this.userRepository.findOrganizationUserByEmailDomain(emailDomain);
+    if (!organizationUser?.id) {
+      throw new HttpError("No organization found with the given email domain", 404);
+    }
+    const organization =
+      await this.organizationRepository.findOrganizationByUserId(
+        organizationUser.id
+      );
+    if (!organization) {
+      throw new HttpError("No organization exists with the same email domain", 404);
+    }
+    const employeeDTO = plainToClass(EmployeeDTO, {
+      userId: createdUser.id,
+      organizationId: organization.id,
+      ...user,
+    });
+    await validateDTO(employeeDTO);
+    try {
+      await this.employeeRepository.createEmployee(employeeDTO, {
+        transaction,
+      });
+    } catch (error: any) {
+      console.log("Failed to create employee", error?.message);
+      throw new HttpError(error?.message || "Failed to create employee ", error?.statusCode || 500);
+    }
+  }
+
+  formatResponse(createdUser: User, token: string) {
+    return {
+      user: {
+        id: createdUser.id,
+        role: createdUser.role,
+        profilePicture: createdUser.profilePicture,
+      },
+      token,
+    };
+  }
+
+  handleError(error: any) {
+    if (error instanceof UniqueConstraintError) {
+      throw new HttpError("User already exists!", 409);
+    }
+    throw error;
+  }
+}
